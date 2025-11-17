2c3e4d9b78f0ee6386b4fb5b349fc7907a557d7f
Rukmal De <rukmaldesilva25@gmail.com>
2025-11-17 16:40:15 +1100

    Inline Sold site files

---
+++ footer.jsx
@@ -0,0 +1,33 @@
+<footer
+  style={{
+    marginTop: 40,
+    borderTop: '1px solid #e5e7eb',
+    paddingTop: 16,
+    fontSize: 12,
+    color: '#6b7280',
+    textAlign: 'center',
+    lineHeight: 1.6,
+  }}
+>
+  <p>
+    Sold Financial is a trading name of <strong>Homei Fi Pty Ltd</strong> (ABN 26 691 328 499).<br />
+    Homei Fi Pty Ltd (Credit Representative Number 573517) is authorised under 
+    <strong>Australian Credit Licence Number 486112 (Purple Circle Financial Services Pty Ltd)</strong>.
+  </p>
+
+  <p>
+    © {new Date().getFullYear()} Homei Fi Pty Ltd. All rights reserved.&nbsp;
+    <a
+      href="/terms"
+      style={{ color: '#000', fontWeight: 600, textDecoration: 'underline', marginRight: 12 }}
+    >
+      Terms
+    </a>
+    <a
+      href="/privacy"
+      style={{ color: '#000', fontWeight: 600, textDecoration: 'underline' }}
+    >
+      Privacy Policy
+    </a>
+  </p>
+</footer>
